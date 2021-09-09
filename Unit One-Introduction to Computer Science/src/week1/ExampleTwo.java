9dad02020307a5ab0ca665cd91f0c3f095988eb4
Bryan_Huang <qhuang@bayviewglen.ca>
2021-09-09 13:17:58 -0400

    Test commit

---
+++ Unit One-Introduction to Computer Science/src/week1/ExampleTwo.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class ExampleTwo {
+    public static void main(String[]args){
+        System.out.println("a");
+        System.out.print("b");
+        System.out.print("c");
+    }
+    
+}
