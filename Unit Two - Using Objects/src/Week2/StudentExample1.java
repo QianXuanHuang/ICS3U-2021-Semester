b498655fc2ad51c3a2896f1fcb40e0baedd8393e
Bryan_Huang <qhuang@bayviewglen.ca>
2021-09-16 17:47:33 -0400

    void method 2.2-2.4

---
+++ Unit Two - Using Objects/src/Week2/StudentExample1.java
@@ -0,0 +1,26 @@
+package Week2;
+
+public class StudentExample1 {
+    public static void main(String[] aStrings){
+        //Ritchie is a varaiable to an instance of the class Student
+        //Ritchie is an Object (instance of a class)
+        Student ritchie = new Student("Ritchie", "45646", 12);
+        Student yola = new Student("Yola", "123456", 10);
+        //ritchie and yola are references to 2 different students
+
+        Student rastin = ritchie;
+        //a void method performs a task and returns NOTHING
+
+        //because rastin and ritche refer to the same Student object it increases the grade for both(still its only one object)
+        rastin.increaseGrade(); 
+
+        ritchie = null;
+        //student tristan; tristan is currently the null refrence
+
+        ritchie.increaseGrade();
+        //throws ajava.long.NullPointerException because ritchie
+
+        rastin.
+    }
+    
+}
